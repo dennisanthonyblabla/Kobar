9bac4f6eb58099e3a5ae057f6a4205920268378f
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-10-24 13:01:42 +0700

    feat: reusable cards added

--- MacroChallengeTeam2/ViewController/ViewController.swift
+++ MacroChallengeTeam2/ViewController/ViewController.swift
@@ -10,33 +10,15 @@ import SwiftUI
 import SnapKit
 
 class ViewController: UIViewController {
-    private lazy var textView: CardView = {
-        let view = CardView()
-        view.cardType = .codingCard
-        view.placeholderText = "Ketik kodingannya di sini"
-        return view
-    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
-        view.addSubview(textView)
-        setupAutoLayout()
-        smallBackButton.addTarget(self, action: #selector(clickBack), for: .touchUpInside)
-    }
-    private func setupAutoLayout() {
-        textView.snp.makeConstraints { (make) in
-            make.width.equalToSuperview().offset(-500)
-            make.height.equalToSuperview().offset(-200)
-            make.center.equalToSuperview()
-        }
     }
 
-    // TODO: add behavior
-    @objc func clickBack() {
-        print("Clicked")
-    }
 }
 struct ViewControllerPreviews: PreviewProvider {
+
     static var previews: some View {
         UIViewControllerPreview {
             return ViewController()
