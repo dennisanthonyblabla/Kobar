0ecddafe701135f8fe25f8bc928a4a9e938f8d0d
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-10-31 12:46:51 +0700

    feat: dropdown button for settings done. please check

--- MacroChallengeTeam2/ViewController/Example Views/DropdownButtonViewController.swift
+++ MacroChallengeTeam2/ViewController/Example Views/DropdownButtonViewController.swift
@@ -11,15 +11,62 @@ import SwiftUI
 
 class DropdownButtonViewController: UIViewController {
 
+    private lazy var ddBG: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 32.5
+        view.alpha = 0.5
+        return view
+    }()
+
+    private lazy var settingBtn = DropDownButtonsView(variant: .variant1)
+    private lazy var soundBtn = DropDownButtonsView(variant: .variant2)
+    private lazy var musicBtn = DropDownButtonsView(variant: .variant3)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
+        view.addSubview(ddBG)
+        view.addSubview(settingBtn)
+        view.addSubview(soundBtn)
+        view.addSubview(musicBtn)
+
+        settingBtn.addTarget(self, action: #selector(settingFunc), for: .touchUpInside)
+        soundBtn.addTarget(self, action: #selector(soundFunc), for: .touchUpInside)
+        musicBtn.addTarget(self, action: #selector(musicFunc), for: .touchUpInside)
         setupAutoLayout()
-        // Do any additional setup after loading the view.
     }
 
     private func setupAutoLayout() {
+        ddBG.snp.makeConstraints { make in
+            make.width.equalTo(65)
+            make.height.equalTo(202)
+            make.center.equalToSuperview()
+        }
+        settingBtn.snp.makeConstraints { make in
+            make.centerX.equalTo(ddBG)
+            make.centerY.equalTo(ddBG.snp.top).offset(33)
+        }
+        soundBtn.snp.makeConstraints { make in
+            make.centerX.equalTo(ddBG)
+            make.top.equalTo(settingBtn.snp.bottom).offset(25.5)
+        }
+        musicBtn.snp.makeConstraints { make in
+            make.centerX.equalTo(ddBG)
+            make.top.equalTo(soundBtn.snp.bottom).offset(25.5)
+        }
+    }
 
+    @objc func settingFunc() {
+        print("ToDoSetting")
+    }
+
+    @objc func soundFunc() {
+        print("ToDoSound")
+    }
+
+    @objc func musicFunc() {
+        print("ToDoMusic")
     }
 }
 
