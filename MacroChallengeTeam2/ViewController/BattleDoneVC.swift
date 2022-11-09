ac519b09e8c282abe10c14c8ec99c6127ceb807c
Atyanta Awesa <awesaatyanta.p@gmail.com>
2022-11-10 02:27:28 +0700

    feat: tanding selesai page done

---
+++ MacroChallengeTeam2/ViewController/BattleDoneVC.swift
@@ -0,0 +1,127 @@
+//
+//  BattleDoneVC.swift
+//  Macro Challenge Team2
+//
+//  Created by Atyanta Awesa Pambharu on 10/11/22.
+//
+
+import UIKit
+import SwiftUI
+import Lottie
+
+class BattleDoneVC: UIViewController {
+    private var seconds = 90
+    private var timer = Timer()
+    private var isTimerRunning = false
+
+    private lazy var testCaseBtn = MedButtonView(
+        variant: .variant2,
+        title: "Test Case",
+        width: 200)
+
+    private lazy var background: UIView = {
+        let view = UIView()
+        view.backgroundColor = .kobarBlueBG
+        return view
+    }()
+
+    private lazy var backgroundMotives: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.image = UIImage(named: "background4")
+        return view
+    }()
+
+    private lazy var toaGif: LottieAnimationView = {
+        let jsonName = "Toa"
+        let animation = LottieAnimation.named(jsonName)
+        let gif = LottieAnimationView(animation: animation)
+        gif.contentMode = .scaleAspectFit
+        gif.loopMode = .loop
+        gif.play()
+        gif.backgroundBehavior = .pauseAndRestore
+        return gif
+    }()
+
+    private lazy var heading: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Mantap! Dah Kelar Tandingnya!"
+        label.font = .bold34
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var desc: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        let descContent = NSAttributedString("Sekarang, liat uji kasus buat cek kodingan lo\ntadi bener atau salah.")
+            .withLineSpacing(3)
+        label.attributedText = descContent
+        label.textColor = .white
+        label.font = .regular28
+        label.textAlignment = .center
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(background)
+        view.addSubview(backgroundMotives)
+        view.addSubview(toaGif)
+        view.addSubview(testCaseBtn)
+        view.addSubview(heading)
+        view.addSubview(desc)
+
+        setupBackground()
+        setupDisplays()
+        setupComponents()
+    }
+
+    private func setupBackground() {
+        background.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().offset(50)
+            make.center.equalToSuperview()
+        }
+        backgroundMotives.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+            make.center.equalToSuperview()
+        }
+    }
+
+    private func setupDisplays() {
+        heading.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(90)
+        }
+        desc.snp.makeConstraints { make in
+            make.centerX.equalTo(heading)
+            make.top.equalTo(heading.snp.bottom).offset(19)
+        }
+        toaGif.snp.makeConstraints { make in
+            make.width.equalTo(400)
+            make.height.equalTo(400)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(heading.snp.top).offset(-5)
+        }
+    }
+
+    private func setupComponents() {
+        testCaseBtn.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-100)
+        }
+    }
+}
+
+struct BattleDoneVCPreviews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview {
+            return BattleDoneVC()
+        }
+        .previewDevice("iPad Pro (11-inch) (3rd generation)").previewInterfaceOrientation(.landscapeLeft)
+    }
+}
